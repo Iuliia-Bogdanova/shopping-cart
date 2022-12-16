3623990c2e8a81f8f76a9c5e72450f459c0585ff
Iuliia-Bogdanova <u-bogdanova@yandex.ru>
2022-12-16 18:42:16 +0400

    doing

--- script.js
+++ script.js
@@ -3,13 +3,18 @@ document.addEventListener("DOMContentLoaded", function (event) {
 });
 
 function showCat() {
-    fetch("https://api.giphy.com/v1/gifs/search?api_key=YPIGxAd3kqGpf7H7GBpW2RAwfB7eLxXA&q=cat&limit=50&offset=0&rating=g&lang=en")
+    fetch("https://api.giphy.com/v1/gifs/search?api_key=YPIGxAd3kqGpf7H7GBpW2RAwfB7eLxXA&q=cat&limit=5&offset=0&rating=g&lang=en")
     .then(response => response.json())
     .then(cat => {
-        /*const catGif = `
-        <div class="gif">${cat.data.images.url}</div>
-        `*/
-        document.getElementById("cat");       console.log(cat);
+        let result = ('');
+        cat.data.forEach(element => {
+            result += `<img src="${element.images.original.url}">`
+        });
+
+        console.log(cat);
+        document.getElementById("cat");  
+        innerHTML = result;     
     })
     .catch(error => console.log(error));
 }
+
